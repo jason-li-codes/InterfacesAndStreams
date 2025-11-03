66d92f170bbcdf218ba8d0f58bd3d279129fa475
lijas <lijason991@gmail.com>
2025-11-02 22:26:43 -0500

    added comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // List of Persons initialized as ArrayList
         List<Person> myFriends = new ArrayList<>();
+        // manually add Persons to ArrayList
         myFriends.add(new Person("Jason", "Li", 92));
         myFriends.add(new Person("Alwin", "Zhao", 25));
         myFriends.add(new Person("Ahsan", "Baseer", 36));
@@ -18,7 +20,9 @@ public class Main {
         myFriends.add(new Person("Andy", "Ariza", 20));
         myFriends.add(new Person("Denus", "Leon", 12));
 
+        // now that compareTo has been implemented in the class, Collections.sort() should work properly
         Collections.sort(myFriends);
+        // after sorting, prints out each person's name and age to verify
         for (Person p : myFriends) {
             System.out.println(p.getFirstName() + " " + p.getLastName() + ", " + p.getAge());
         }
--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,17 +1,21 @@
 package com.pluralsight;
 
+// implements Comparable interface for sorting
 public class Person implements Comparable<Person> {
 
+    // private fields
     private String firstName;
     private String lastName;
     private int age;
 
+    // constructor
     public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
 
+    // getters and setters
     public String getFirstName() {
         return firstName;
     }
@@ -32,20 +36,24 @@ public class Person implements Comparable<Person> {
         return age;
     }
 
+    // Overridden compareTo method to compare Persons for sorting
     @Override
     public int compareTo(Person p) {
-
+        // checks if last names are identical
         if (lastName.compareTo(p.lastName) == 0) {
+            // checks if first names are identical
             if (firstName.compareTo(p.firstName) == 0) {
+                // checks if ages are identical
                 if (age == p.getAge()) {
-                    return 0;
+                    return 0; // if all these are true, return a 0
                 } else {
+                    // otherwise, return 1 or -1, with the older Person getting priority
                     return (age > p.getAge()) ? 1 : -1;
                 }
-            } else {
+            } else { // if last names are identical but first names are not, compare first names
                 return firstName.compareTo(p.firstName);
             }
-        } else {
+        } else { // if last names are not identical, compare last names
             return lastName.compareTo(p.lastName);
         }
     }
