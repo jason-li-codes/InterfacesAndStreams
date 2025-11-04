e74a45310b4d9b2b0519223e12e5367316a9c837
jasonli <lijason991@gmail.com>
2025-11-04 09:25:03 -0500

    added traditional forEach loops for searching

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,8 +3,11 @@ package com.pluralsight;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
 
@@ -19,6 +22,8 @@ public class Main {
         myFriends.add(new Person("Ashley", "Mendez", 21));
         myFriends.add(new Person("Andy", "Ariza", 20));
         myFriends.add(new Person("Denus", "Leon", 12));
+        myFriends.add(new Person("Tamir", "Dmitriev", 19));
+        myFriends.add(new Person("Roger", "Su", 12));
 
         // now that compareTo has been implemented in the class, Collections.sort() should work properly
         Collections.sort(myFriends);
@@ -26,6 +31,57 @@ public class Main {
         for (Person p : myFriends) {
             System.out.println(p.getFirstName() + " " + p.getLastName() + ", " + p.getAge());
         }
+
+        System.out.println("Who are you searching for? Enter first or last name.");
+        String userSearch = getValidString().toLowerCase();
+        System.out.println("Here are people matching your search: ");
+        for (Person p: myFriends) {
+            if (p.getFirstName().toLowerCase().contains(userSearch) ||
+            p.getLastName().toLowerCase().contains(userSearch)) {
+                System.out.println(p.getFirstName() + " " + p.getLastName() + ", " + p.getAge());
+            }
+        }
+        System.out.println("Search complete.");
+
+        int counter = 0;
+        int ageTotal = 0;
+        for (Person p : myFriends) {
+            counter++;
+            ageTotal += p.getAge();
+        }
+        System.out.printf("Average age of group: %d\n", ageTotal / counter);
+
+        int ageMin = Integer.MAX_VALUE;
+        int ageMax = 0;
+        for (Person p : myFriends) {
+            if (p.getAge() < ageMin) {
+                ageMin = p.getAge();
+            }
+            if (p.getAge() > ageMax) {
+                ageMax = p.getAge();
+            }
+        }
+
+        System.out.printf("The youngest person in the group is %d years old.", ageMin);
+        System.out.printf("The oldest person in the group is %d years old.", ageMax);
+
+    }
+
+    public static String getValidString() {
+        // Initializes String called string, set to null
+        String string;
+        // Uses do/while loop with a boolean badInput to get a non-empty String from user
+        boolean badInput = false;
+        do {
+            // Sets badInput to false first, to ensure loop doesn't run continuously
+            badInput = false;
+            string = input.nextLine().trim(); // Accepts next user input as a String, trimming it
+            if (string.isEmpty()) {
+                System.out.println("You have not entered anything, please try again.");
+                badInput = true; // Sets badInput to true if input is empty after trimming
+            }
+        } while (badInput);
+        return string; // Returns string after input is confirmed to be non-empty
     }
 
 }
